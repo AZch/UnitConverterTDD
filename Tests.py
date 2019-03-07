08f263b6ba164430b0d2b5869a407692d35b20cf
azch <antonzhuikov@gmail.com>
2019-03-07 23:00:18 +0700

    add test check list name by type list

--- Tests.py
+++ Tests.py
@@ -6,6 +6,10 @@ class TestMainConvert(unittest.TestCase):
         converter = Converter()
         self.assertIsNotNone(converter, "converter cant be None")
 
-    def testCheckQuantitiesNotNone(self):
+    def testCheckNamesNotNone(self):
         converter = Converter()
         self.assertIsNotNone(converter.getNames(), "names cant be None")
+
+    def testNamesIsArr(self):
+        converter = Converter()
+        self.assertIsInstance(converter.getNames(), list, "name shoud be type list")
