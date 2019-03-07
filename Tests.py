d81526e88f92b451c709b46bde790e0446b05309
azch <antonzhuikov@gmail.com>
2019-03-07 22:53:56 +0700

    add test Names quantities not None

--- Tests.py
+++ Tests.py
@@ -5,3 +5,7 @@ class TestMainConvert(unittest.TestCase):
     def testConverterNotNone(self):
         converter = Converter()
         self.assertIsNotNone(converter, "converter cant be None")
+
+    def testCheckQuantitiesNotNone(self):
+        converter = Converter()
+        self.assertIsNotNone(converter.getNames, "names cant be None")
