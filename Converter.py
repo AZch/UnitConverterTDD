816c4f0b23be5d2c5390dccc2e066e19f2c3ff69
azch <antonzhuikov@gmail.com>
2019-03-08 14:02:19 +0700

    passes test testSimilarWayQuant100_EmptyName_RandomListWithMoreZero

--- Converter.py
+++ Converter.py
@@ -7,6 +7,7 @@ class Converter():
         if len(self.__quantitiesMult) == 0:
             self.__quantitiesMult.append([1])
         else:
+            arrFromQuant = self.__checkSimilarWay(arrFromQuant)
             if len(arrFromQuant) > len(self.__quantitiesMult):
                 arrFromQuant[len(arrFromQuant) - 1] = 1
             else:
@@ -14,6 +15,15 @@ class Converter():
             self.__quantitiesMult.append(arrFromQuant)
             self.__appendNewWay(arrFromQuant)
         self.__names.append(name)
+
+    def __checkSimilarWay(self, arrFromQuant):
+        for i in range(len(arrFromQuant)):
+            if arrFromQuant[i] == 0:
+                for j in range(len(arrFromQuant)):
+                    if arrFromQuant[j] != 0 and self.__quantitiesMult[i][j] != 0:
+                        arrFromQuant[i] = arrFromQuant[j] / self.__quantitiesMult[i][j]
+                        break
+        return arrFromQuant
 
     def __appendNewWay(self, arrFromQuant):
         for i in range(len(self.__quantitiesMult) - 1):
