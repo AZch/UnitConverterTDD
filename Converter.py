1586ee705a58a5b9dca32f588637528b2bc11e60
azch <antonzhuikov@gmail.com>
2019-03-08 12:21:47 +0700

    passess test testCheckQunat100_RandomName_randomList

--- Converter.py
+++ Converter.py
@@ -1,12 +1,14 @@
 class Converter():
     def __init__(self):
         self.__names = []
+        self.__quantitiesMult = []
 
     def addQuantities(self, name, arrFromQuant):
+        self.__quantitiesMult.append(arrFromQuant)
         self.__names.append(name)
 
     def getNames(self):
         return self.__names
 
     def getQuantities(self):
-        return [[1]]
+        return self.__quantitiesMult
