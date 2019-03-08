2a17fa7f41959f6c90b91df646cb46edb01657c3
azch <antonzhuikov@gmail.com>
2019-03-08 12:38:09 +0700

    passess testMainDiadQuant100_EmptyName_randomList

--- Converter.py
+++ Converter.py
@@ -7,6 +7,10 @@ class Converter():
         if len(self.__quantitiesMult) == 0:
             self.__quantitiesMult.append([1])
         else:
+            if len(arrFromQuant) > len(self.__quantitiesMult):
+                arrFromQuant[len(arrFromQuant) - 1] = 1
+            else:
+                arrFromQuant.append(1)
             self.__quantitiesMult.append(arrFromQuant)
             for i in range(len(self.__quantitiesMult) - 1):
                 self.__quantitiesMult[i].append(0)
